3dc6cf4bb5cde7c061c4ece9c5f0591912a2dcc2
zied <zied@Latitude>
2016-11-25 13:56:44 +0100

    [Creating project modules]:By Sendi Zied

---
+++ Predict-stock-market/Product-Kafka/src/main/java/org/Product/Kafka/App.java
@@ -0,0 +1,13 @@
+package org.Product.Kafka;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Predict-stock-market/src/main/java/org/tcb/Predict_stock_market/App.java
@@ -0,0 +1,13 @@
+package org.tcb.Predict_stock_market;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
